819bc5b4979f99ba34dfd60113fd67d8afb43ee8
jonesa6 <156232904+jonesa6@users.noreply.github.com>
2024-05-16 12:54:30 -0700

    giant changes. tilemap perfomance etc

--- main.ts
+++ main.ts
@@ -1,18 +1,105 @@
+namespace SpriteKind {
+    export const fire = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     for (let index = 0; index < 5; index++) {
         mySprite.y += -5
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
+    info.changeLifeBy(-1)
+    pause(100)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (pc > 0 && info.life() < 3) {
+        info.changeLifeBy(1)
+        pc += -1
+    }
+})
+controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
+    if (true) {
+        mySprite.ay = 100
+    } else {
+        mySprite.ay = 200
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (info.score() > 8) {
+        info.changeScoreBy(-8)
+        pc += 1
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+    info.changeScoreBy(3)
+})
+controller.combos.attachCombo("L+R", function () {
+    if (info.player3.score() > 2) {
+        if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.FacingLeft, Predicate.NotMoving))) {
+            mySprite3 = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 4 4 4 4 . . . . . . 
+                . . . . 4 4 4 5 5 4 4 4 . . . . 
+                . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+                . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+                . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+                . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+                . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+                . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+                . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+                . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+                . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+                . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+                . . . . 4 4 2 2 2 2 4 4 . . . . 
+                . . . . . . 4 4 4 4 . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.fire)
+            mySprite3.setPosition(mySprite.x, mySprite.y)
+            mySprite3.vx = -100
+        }
+        if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.FacingRight, Predicate.NotMoving))) {
+            mySprite3 = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 4 4 4 4 . . . . . . 
+                . . . . 4 4 4 5 5 4 4 4 . . . . 
+                . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+                . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+                . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+                . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+                . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+                . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+                . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+                . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+                . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+                . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+                . . . . 4 4 2 2 2 2 4 4 . . . . 
+                . . . . . . 4 4 4 4 . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.fire)
+            mySprite3.setPosition(mySprite.x, mySprite.y)
+            mySprite3.vx = 100
+        }
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level2`)
     mySprite.x = 10
     mySprite.y = 32
+    mySprite.sayText("loading", 5000, false)
+    timer.after(10, function () {
+        mySprite.sayText("loaded", 5000, false)
+    })
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
     sprites.destroyAllSpritesOfKind(SpriteKind.Food)
     mySprite.x = randint(255, 600)
     mySprite.y = 96
     tiles.setCurrentTilemap(tilemap`level4`)
+    mySprite.sayText("loading", 5000, false)
+    timer.after(10, function () {
+        mySprite.sayText("loaded", 5000, false)
+    })
 })
 sprites.onOverlap(SpriteKind.Food, SpriteKind.Player, function (sprite, otherSprite) {
     if (Math.percentChance(50)) {
@@ -21,6 +108,13 @@ sprites.onOverlap(SpriteKind.Food, SpriteKind.Player, function (sprite, otherSpr
         mySprite2.ax = -200
     }
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, location) {
+    game.setGameOverMessage(false, "you fell out of the world")
+    game.gameOver(false)
+})
+scene.onOverlapTile(SpriteKind.Food, assets.tile`myTile3`, function (sprite, location) {
+    sprites.destroy(mySprite2)
+})
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (true) {
         mySprite.ay = 400
@@ -28,8 +122,19 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.ay = 200
     }
 })
+info.player1.onLifeZero(function () {
+    game.setGameOverMessage(false, "you died")
+    game.gameOver(false)
+})
+let fall_damage = 0
+let px = 0
+let py = 0
 let mySprite2: Sprite = null
+let mySprite3: Sprite = null
+let pc = 0
+let sc = 0
 let mySprite: Sprite = null
+let seed = randint(1, 10000000)
 mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
@@ -48,8 +153,10 @@ mySprite = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
+mySprite.setVelocity(5, 0)
 controller.moveSprite(mySprite, 100, 0)
 scene.cameraFollowSprite(mySprite)
+sc += 1
 characterAnimations.loopFrames(
 mySprite,
 [img`
@@ -290,18 +397,25 @@ mySprite,
 characterAnimations.rule(Predicate.MovingDown)
 )
 mySprite.ay = 200
+mySprite.x = randint(20, 255)
 mySprite.y = 10
 tiles.setCurrentTilemap(tilemap`level2`)
+info.setLife(3)
+info.setScore(100)
+pc = 0
+info.player3.setScore(100)
 game.onUpdate(function () {
-    if (characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.HittingWallLeft, Predicate.MovingDown)) || characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.HittingWallRight, Predicate.MovingDown))) {
-        if (Math.percentChance(50)) {
-            mySprite2.ax = 200
-        } else {
-            mySprite2.ax = -200
+    if (!(characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.MovingDown)))) {
+        if (characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.HittingWallUp)) || (characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.HittingWallLeft)) || characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.HittingWallRight)))) {
+            if (Math.percentChance(50)) {
+                mySprite2.ax = 200
+            } else {
+                mySprite2.ax = -200
+            }
         }
     }
-    if (characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.NotMoving, Predicate.MovingDown))) {
-        if (Math.percentChance(50)) {
+    if (characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.HittingWallLeft)) || characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.HittingWallRight))) {
+        if (true) {
             mySprite2.ax = 200
         } else {
             mySprite2.ax = -200
@@ -310,6 +424,46 @@ game.onUpdate(function () {
     if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.HittingWallRight)) || characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.HittingWallLeft))) {
         mySprite.y += -5
     }
+    if (characterAnimations.matchesRule(mySprite2, characterAnimations.rule(Predicate.HittingWallRight)) || characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.HittingWallLeft))) {
+        mySprite2.y += -5
+    }
+    // perfomance optimization
+    if (py > scene.screenHeight() - mySprite.y || py < scene.screenHeight() - mySprite.y || (px < scene.screenWidth() - mySprite.x || px > scene.screenWidth() - mySprite.x)) {
+        sprites.destroy(mySprite2)
+    }
+    px = mySprite2.x
+    py = mySprite2.y
+    if (sc > 4) {
+        sprites.destroyAllSpritesOfKind(SpriteKind.Food)
+    }
+    // fall damage
+    if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.MovingDown))) {
+        if (mySprite.vy > 110) {
+            fall_damage = 1
+        }
+        if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.HittingWallDown)) && fall_damage == 1) {
+            info.changeLifeBy(-1)
+        }
+        if (mySprite.vy < 50) {
+            fall_damage = 0
+        }
+    }
+    if (info.life() > 3) {
+        info.changeLifeBy(-1)
+    }
+    info.player2.setScore(pc)
+    if (mySprite3) {
+        if (characterAnimations.matchesRule(mySprite3, characterAnimations.rule(Predicate.HittingWallUp)) || (characterAnimations.matchesRule(mySprite3, characterAnimations.rule(Predicate.HittingWallRight)) || (characterAnimations.matchesRule(mySprite3, characterAnimations.rule(Predicate.HittingWallDown)) || characterAnimations.matchesRule(mySprite3, characterAnimations.rule(Predicate.HittingWallLeft))))) {
+            sprites.destroy(mySprite3)
+        }
+    }
+    info.player2.setLife(info.score() / 8)
+})
+game.onUpdate(function () {
+    if (mySprite.tileKindAt(TileDirection.Bottom, assets.tile`transparency16`)) {
+        mySprite.y += 5
+        mySprite.x += randint(-1, 1)
+    }
 })
 game.onUpdateInterval(5000, function () {
     mySprite2 = sprites.create(img`
@@ -330,8 +484,9 @@ game.onUpdateInterval(5000, function () {
         . . . f f f f f f . . . . 
         . . . f f . . f f . . . . 
         `, SpriteKind.Food)
+    mySprite2.ax = -20
     mySprite2.ay = 200
-    mySprite2.x = randint(1, 255)
+    mySprite2.x = randint(1, 255) / seed * 2
     characterAnimations.loopFrames(
     mySprite2,
     [img`
@@ -378,7 +533,7 @@ game.onUpdateInterval(5000, function () {
     500,
     characterAnimations.rule(Predicate.MovingLeft)
     )
-    mySprite2.lifespan = 10000
+    sc += 1
 })
 game.onUpdateInterval(5000, function () {
     mySprite2 = sprites.create(img`
@@ -399,8 +554,9 @@ game.onUpdateInterval(5000, function () {
         . . . f f f f f f . . . . 
         . . . f f . . f f . . . . 
         `, SpriteKind.Food)
+    mySprite2.ax = 60
     mySprite2.ay = 200
-    mySprite2.x = randint(1, 255)
+    mySprite2.x = randint(1, 255) / seed * 2
     characterAnimations.loopFrames(
     mySprite2,
     [img`
@@ -447,5 +603,10 @@ game.onUpdateInterval(5000, function () {
     500,
     characterAnimations.rule(Predicate.MovingLeft)
     )
-    mySprite2.lifespan = 10000
+    sc += 1
+})
+game.onUpdateInterval(5000, function () {
+    if (info.life() < 3) {
+        info.changeLifeBy(1)
+    }
 })
